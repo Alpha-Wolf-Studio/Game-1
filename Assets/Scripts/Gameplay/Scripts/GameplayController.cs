fd9582542271a5bf8fe01596c5633a3b4c4fb3f5
Mauro <mauro.mendozacp@gmail.com>
2023-09-29 23:10:58 -0300

    Add gameplay controller base wip

---
+++ Assets/Scripts/Gameplay/Scripts/GameplayController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    //Agregar Handlers
+
+
+    // Llamar a los Init de cada Sytem/Handler
+    void Start()
+    {
+        
+    }
+}
